491ed3d15474adfe113bae581d46c22c7e193909
samuelet23 <samueletoscano@hotmail.com>
2024-02-16 17:16:53 +0100

    aggiunto id per database

--- src/main/java/progetto_settimanale/gestione_dispositivi/model/Dispositivo/Dispositivo.java
+++ src/main/java/progetto_settimanale/gestione_dispositivi/model/Dispositivo/Dispositivo.java
@@ -9,6 +9,11 @@ import progetto_settimanale.gestione_dispositivi.model.Type.StatoDispositivo;
 @Data
 public class Dispositivo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dispositivo_id")
+    @SequenceGenerator(name = "dispositivo_id", initialValue = 0, allocationSize = 1)
+    private int id;
+
     @Enumerated(EnumType.STRING)
     private StatoDispositivo statoDispositivo;
 
