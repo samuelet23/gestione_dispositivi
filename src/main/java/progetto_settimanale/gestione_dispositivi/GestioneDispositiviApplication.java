63ab130a6acaf0236b47180245a0b6e6c4c2e4a0
samuelet23 <samueletoscano@hotmail.com>
2024-02-16 17:04:59 +0100

    final commit

--- src/main/java/progetto_settimanale/gestione_dispositivi/GestioneDispositiviApplication.java
+++ src/main/java/progetto_settimanale/gestione_dispositivi/GestioneDispositiviApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestioneDispositiviApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GestioneDispositiviApplication.class, args);
 	}
 
